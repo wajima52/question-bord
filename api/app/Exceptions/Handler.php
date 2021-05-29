01a25e19bb8a357a16a3b47b177efb63df62c6fa
wajima <mikkywaji@icloud.com>
2021-05-30 08:26:43 +0900

    エラー時のレスポンスをjsonに修正

--- api/app/Exceptions/Handler.php
+++ api/app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -37,5 +38,17 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $error)
+    {
+        if ($request->is('api/*')) {
+            return response()->json([
+                'status' => Response::HTTP_BAD_REQUEST,
+                'errors' => $error->getMessage()
+            ]);
+        }
+
+        return parent::render();
     }
 }
