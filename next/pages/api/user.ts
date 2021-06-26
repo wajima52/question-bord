a0bb861ea2137a77d13f5e534a513f2427ceaffd
wajima <mikkywaji@icloud.com>
2021-06-26 14:05:21 +0900

    ユーザー情報取得処理のnext側を実装

--- next/pages/api/user.ts
+++ next/pages/api/user.ts
@@ -3,12 +3,12 @@ import { get } from "../../utils/helpers/client"
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const apiResponse = await get(process.env.BACKEND_URL + "/user", {
+    const user = await get(process.env.BACKEND_URL + "/user", {
       headers: new Headers(req.headers),
     })
-    res.status(200).json({ hoge: "huge" })
+    res.status(200).json({ isLoggedIn: true, ...user })
   } catch (error: Error) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    res.status(Number(error.message)).json("login error")
+    res.status(Number(error.message)).json({ isLoggedIn: false })
   }
 }
--- next/utils/hooks/useUser.ts
+++ next/utils/hooks/useUser.ts
@@ -1,11 +1,33 @@
 import Error from "next/error"
+import { Router } from "next/router"
+import { parseCookies } from "nookies"
+import { useEffect } from "react"
 import useSWR from "swr"
 import { get } from "../helpers/client"
 import { User } from "../interfaces/User"
 
-const fetcher = (url: string) => get(url)
+const fetcher = (url: string) => {
+  const cookie = parseCookies()
+  return get(url, {
+    headers: new Headers({
+      Authorization: `Bearer ${cookie["token"]}`,
+    }),
+  })
+}
 
-export const useUser = () => {
-  const { data, error } = useSWR<User, Error>("/api/user", fetcher)
-  console.log(data)
+export const useUser = ({ redirectTo = false, redirectIfFound = false }) => {
+  const { data: user } = useSWR<User, Error>("/api/user", fetcher)
+
+  useEffect(() => {
+    if (!redirectTo || !user) return
+
+    if (
+      (redirectTo && !redirectIfFound && !user?.isLoggedIn) ||
+      (redirectIfFound && user?.isLoggedIn)
+    ) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      Router.push(redirectTo)
+    }
+  }, [user, redirectIfFound, redirectTo])
+  return user
 }
